246dd38b91b5af6ede054d22769d93c01cc7bb34
Rmjp <rmjp15.rmt@gmail.com>
2023-08-28 22:42:26 +0700

    css init

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,21 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<div class="h-screen">
+    <div class="flex flex-row flex-wrap h-full">
+        <div class="w-full sm:w-1/3 md:w-1/4 px-2 h-full">
+            nav
+        </div>
+
+        <div class="w-full sm:w-2/3 md:w-3/4 pt-1 px-2 h-full">
+            <div class="h-10 py-3">
+                menu
+            </div>
+
+            <div class="h-3/4">
+                editor
+            </div>
+
+            <div>
+                console
+            </div>
+        </div>
+    </div>
+</div>

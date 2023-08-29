65d8eff83ba7555c92c6a3bccab6a183690be78b
Rmjp <rmjp15.rmt@gmail.com>
2023-08-29 15:01:58 +0700

    add: codemirror

---
+++ src/routes/test/+page.svelte
@@ -0,0 +1,20 @@
+<script lang="ts">
+    import {EditorView, basicSetup} from "codemirror"
+    import {cpp} from "@codemirror/lang-cpp"
+    import { onMount, onDestroy } from 'svelte';
+
+    onMount(() => {
+        let editor = new EditorView({
+            extensions: [basicSetup, cpp()],
+            parent: document.body
+        })
+
+        // Cleanup the event listener when the component is destroyed
+        onDestroy(() => {
+            
+        });
+    });
+</script>
+<body>
+
+</body>

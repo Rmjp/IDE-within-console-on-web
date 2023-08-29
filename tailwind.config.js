a5bc021326aae5c5536dafcda3724f6f204756f0
Rmjp <rmjp15.rmt@gmail.com>
2023-08-29 15:02:21 +0700

    add:codemirror

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,25 @@
-/** @type {import('tailwindcss').Config} */
-export default {
-  content: ['./src/**/*.{html,js,svelte,ts}'],
+const config = {
+  content: ['./src/**/*.{html,js,svelte,ts}', './node_modules/flowbite-svelte/**/*.{html,js,svelte,ts}'],
+
+  plugins: [require('flowbite/plugin')],
+
+  darkMode: 'class',
+
   theme: {
-    extend: {}
-  },
-  plugins: []
+    extend: {
+      colors: {
+        // flowbite-svelte
+        primary: {
+          50: '#FFF5F2',
+          500: '#EDD3C4',
+          600: '#C8ADC0',
+          700: '#7765E3',
+          800: '#3B60E4',
+          900: '#080708'
+        }
+      }
+    }
+  }
 };
+
+module.exports = config;

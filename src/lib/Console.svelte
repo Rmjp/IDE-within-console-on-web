0975adaadfa16a76012da685c61650d6340b77ba
Rmjp <rmjp15.rmt@gmail.com>
2023-09-24 23:07:24 +0700

    docker

--- src/lib/Console.svelte
+++ src/lib/Console.svelte
@@ -22,9 +22,29 @@
         }, delayInMilliseconds);
         
     }
+
+    function loadScript(src) {
+    return new Promise((resolve, reject) => {
+      const script = document.createElement('script');
+      script.src = src;
+
+      script.onload = () => {
+        resolve();
+      };
+
+      script.onerror = (error) => {
+        reject(new Error(`Script load error for ${src}`));
+      };
+
+      document.body.appendChild(script);
+    });
+  }
+
     onMount(async () => {
-        await import('$lib/v86/build/xterm.js');
-        await import('$lib/v86/build/libv86.js');
+        await loadScript('/v86/build/xterm.js');
+        await loadScript('/v86/build/libv86.js');
+        // await import('/v86/build/xterm.js');
+        // await import('/v86/build/libv86.js');
         const { FitAddon } = await import('xterm-addon-fit');
         const { Terminal } = await import('xterm');
         const fitAddon = new FitAddon();
@@ -36,7 +56,7 @@
         fitAddon.fit();
         
         emulator = new V86Starter({
-            wasm_path: "src/lib/v86/build/v86.wasm",
+            wasm_path: "/v86/build/v86.wasm",
             memory_size: 512 * 1024 * 1024,
             vga_memory_size: 8 * 1024 * 1024,
             // screen_container: screen_container,
@@ -44,7 +64,7 @@
             disable_keyboard: true,
             disable_mouse: true,
             bios: {
-                url: "src/lib/v86/bios/seabios.bin",
+                url: "/v86/bios/seabios.bin",
             },
             // vga_bios: {
             //     url: "src/lib/v86/bios/vgabios.bin",
@@ -60,8 +80,8 @@
                 //     // # See the `disk_size` item in the packer template.
                 // },
                 filesystem: {
-                    baseurl: "src/lib/v86/images/arch/",
-                    basefs: "src/lib/v86/images/fs.json",
+                    baseurl: "/v86/images/arch/",
+                    basefs: "/v86/images/fs.json",
                 },
                 
                 // bzimage_initrd_from_filesystem: true,
@@ -74,7 +94,7 @@
                 acpi: false,
                 autostart: true,
                 initial_state: {
-                    "url": "src/lib/v86/images/v86state.bin.zst",
+                    "url": "/v86/images/v86state.bin.zst",
                 },
         }); 
 
@@ -89,5 +109,9 @@
 
 <div bind:this={terminal_div} id="terminal_div" class="h-full"></div>
 
-<link rel="stylesheet" href="src/lib/v86/build/xterm.css" />
 
+<svelte:head>
+    <link rel="stylesheet" href="/v86/build/xterm.css" />
+	<!-- <script src="/v86/build/xterm.js" ></script>
+    <script src="/v86/build/libv86.js"></script> -->
+</svelte:head>

b32d917d875456e18549ec3ff1015f883fa18c77
Rmjp <rmjp15.rmt@gmail.com>
2023-09-24 21:42:56 +0700

    edit: to local path

--- src/lib/Console.svelte
+++ src/lib/Console.svelte
@@ -1,4 +1,4 @@
-<script>
+<script lang="js">
   import { version } from '$app/environment';
     import { onMount } from 'svelte';
     var terminal_div = null;
@@ -23,8 +23,8 @@
         
     }
     onMount(async () => {
-        await import('https://adt-s3.runnakjeen.com/build/xterm.js');
-        await import('https://adt-s3.runnakjeen.com/build/libv86.js');
+        await import('$lib/v86/build/xterm.js');
+        await import('$lib/v86/build/libv86.js');
         const { FitAddon } = await import('xterm-addon-fit');
         const { Terminal } = await import('xterm');
         const fitAddon = new FitAddon();
@@ -36,46 +36,46 @@
         fitAddon.fit();
         
         emulator = new V86Starter({
-        wasm_path: "https://adt-s3.runnakjeen.com/build/v86.wasm",
-        memory_size: 512 * 1024 * 1024,
-        vga_memory_size: 8 * 1024 * 1024,
-        // screen_container: screen_container,
-        serial_container_xtermjs: terminal,
-        disable_keyboard: true,
-        disable_mouse: true,
-        bios: {
-            url: "https://adt-s3.runnakjeen.com/bios/seabios.bin",
-        },
-        // vga_bios: {
-        //     url: "src/lib/v86/bios/vgabios.bin",
-        // },
-        // hda: {
-            //     url: "../output/images/arch.img",
-            //     // # set to true if you want to load it asynchrously during runtime (for this option we need to run a webserver that supports the Range header)
-            //     // # NOTE: async: false is slow but proved to be more realiable
-            //     async: false,
-            
-            //     // # This needs to be the size of the raw disk.
-            //     size: 1.5 * 1024 * 1024 * 1024,
-            //     // # See the `disk_size` item in the packer template.
+            wasm_path: "src/lib/v86/build/v86.wasm",
+            memory_size: 512 * 1024 * 1024,
+            vga_memory_size: 8 * 1024 * 1024,
+            // screen_container: screen_container,
+            serial_container_xtermjs: terminal,
+            disable_keyboard: true,
+            disable_mouse: true,
+            bios: {
+                url: "src/lib/v86/bios/seabios.bin",
+            },
+            // vga_bios: {
+            //     url: "src/lib/v86/bios/vgabios.bin",
             // },
-            filesystem: {
-                baseurl: "https://adt-s3.runnakjeen.com/images/arch/",
-                basefs: "https://adt-s3.runnakjeen.com/images/fs.json",
-            },
+            // hda: {
+                //     url: "../output/images/arch.img",
+                //     // # set to true if you want to load it asynchrously during runtime (for this option we need to run a webserver that supports the Range header)
+                //     // # NOTE: async: false is slow but proved to be more realiable
+                //     async: false,
+                
+                //     // # This needs to be the size of the raw disk.
+                //     size: 1.5 * 1024 * 1024 * 1024,
+                //     // # See the `disk_size` item in the packer template.
+                // },
+                filesystem: {
+                    baseurl: "src/lib/v86/images/arch/",
+                    basefs: "src/lib/v86/images/fs.json",
+                },
+                
+                // bzimage_initrd_from_filesystem: true,
+                
+                cmdline: [
+                    "rw",
+                "root=host9p rootfstype=9p rootflags=trans=virtio,cache=loose",
+            ].join(" "),
             
-            // bzimage_initrd_from_filesystem: true,
-            
-            cmdline: [
-                "rw",
-            "root=host9p rootfstype=9p rootflags=trans=virtio,cache=loose",
-        ].join(" "),
-        
-            acpi: false,
-            autostart: true,
-            initial_state: {
-                "url": "https://adt-s3.runnakjeen.com/images/v86state.bin.zst",
-            },
+                acpi: false,
+                autostart: true,
+                initial_state: {
+                    "url": "src/lib/v86/images/v86state.bin.zst",
+                },
         }); 
 
         terminal.write("Welcome to console!\r\n");
@@ -89,5 +89,5 @@
 
 <div bind:this={terminal_div} id="terminal_div" class="h-full"></div>
 
-<link rel="stylesheet" href="https://adt-s3.runnakjeen.com/build/xterm.css" />
+<link rel="stylesheet" href="src/lib/v86/build/xterm.css" />
 
